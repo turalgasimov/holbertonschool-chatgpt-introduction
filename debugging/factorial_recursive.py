cfaab785f649908d3e6523b4c1ae01377b107253
turalgasimov <turallgasimov@gmail.com>
2026-03-11 10:55:00 +0000

    add recursive factorial comments

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+'''
+ * factorial - recursiev factorial function
+ * @n: input
+ * Return: factorial(n); 
+'''
+def factorial(n):
+        if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
